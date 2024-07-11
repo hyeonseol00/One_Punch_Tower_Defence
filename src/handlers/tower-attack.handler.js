b7970285dad4c6ea1715f3ac5f4e3f4d7fa56613
hyeonseol00 <hyeonseol22@naver.com>
2024-07-11 19:22:12 +0900

    🐛 bugfix : emit 이벤트명 수정

--- src/handlers/tower-attack.handler.js
+++ src/handlers/tower-attack.handler.js
@@ -1,5 +1,5 @@
 export const towerAttackHandler = async (userId, payload, socket) => {
-  socket.to('gameSession').emit('opponentMonsterSpawn', {
+  socket.to('gameSession').emit('opponentTowerAttack', {
     status: 'success',
     message: '상대 타워가 몬스터를 공격했습니다.',
     data: { towerIdx: payload.towerIdx, monsterIdx: payload.monsterIdx },
