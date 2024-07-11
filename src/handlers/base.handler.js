0b043ec8d6fa239033cc683e4c14163e8a72f1d8
hyeonseol00 <hyeonseol22@naver.com>
2024-07-11 20:44:39 +0900

    ✨ update : 기지 피격 핸들러 업데이트

--- src/handlers/base.handler.js
+++ src/handlers/base.handler.js
@@ -1,14 +1,23 @@
 import { getUserData, updateUserData } from '../models/user-data.model.js';
 
-export const baseUnderAttack = async (userId, payload) => {
+export const baseUnderAttackHandler = async (userId, payload, socket) => {
   const userData = await getUserData(userId);
 
   if (userData.hp < payload.hp)
     return { status: 'fail', message: '기지 체력 데이터가 잘못되었습니다!' };
 
-  userData.hp = payload.hp;
+  userData.hp -= payload.damage;
 
   await updateUserData(userData);
 
-  return { status: 'success', message: '기지가 피격당했습니다!' };
+  socket.emit('baseHitted', {
+    status: 'success',
+    message: '기지가 피격당했습니다!',
+    data: { hp: userData.hp },
+  });
+  socket.to('gameSesseion').emit('opponentBaseHitted', {
+    status: 'success',
+    message: '상대 기지가 피격당했습니다!',
+    data: { opponentHp: userData.hp },
+  });
 };
