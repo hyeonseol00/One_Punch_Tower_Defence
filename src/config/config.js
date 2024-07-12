80e1a72d47b7ab351bbcffd3cc83653e44dd48a8
hyeonseol00 <hyeonseol22@naver.com>
2024-07-12 17:53:55 +0900

    🎉 add : 환경변수 정의

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+import { RESOLUTION_HEIGHT, RESOLUTION_WIDTH } from '../constants/canvas.js';
+
+export const config = {
+  canvas: {
+    width: RESOLUTION_WIDTH,
+    height: RESOLUTION_HEIGHT,
+  },
+};
