4c3ce14c05146336d240afa69887cda9791922c0
KR-EGOIST <jhso5041@naver.com>
2024-07-17 14:59:39 +0900

    ✨ update : 게임 종료 시 유저의 점수도 같이 저장
    resolve #76

--- src/models/history.model.js
+++ src/models/history.model.js
@@ -1,10 +1,12 @@
 import { prisma } from '../utils/prisma/index.js';
 
-export const createHistory = async (userIdA, userIdB) => {
+export const createHistory = async (userIdA, scoreA, userIdB, scoreB) => {
   await prisma.match_history.create({
     data: {
       userIdA: userIdA,
+      scoreA: scoreA,
       userIdB: userIdB,
+      scoreB: scoreB,
       userIdWinner: userIdB,
     },
   });
